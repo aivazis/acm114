6b229cd7486c5a5b3a849f3814f1fe4a196b47a7
Michael Aivazis <michael.aivazis@orthologue.com>
2010-01-04 19:32:43 -0800

    Created a beamer lecture stub

---
+++ lectures/Make.mm
@@ -0,0 +1,69 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             michael a.g. aïvázis
+#                      california institute of technology
+#                      (c) 1998-2010  all rights reserved
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+PROJECT = acm114
+PACKAGE = lectures
+
+RECURSE_DIRS = \
+   figures \
+   listings \
+
+OTHERS = \
+
+#--------------------------------------------------------------------------
+#
+
+DOCUMENT = acm114
+
+PACKAGES =
+
+INCLUDES = \
+    setup.tex \
+    references.bib
+
+SECTIONS = \
+
+FIGURES = \
+
+#--------------------------------------------------------------------------
+#
+
+all: $(DOCUMENT).pdf
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+
+#--------------------------------------------------------------------------
+#
+
+# preview types
+osx: $(DOCUMENT).pdf
+	open $(DOCUMENT).pdf
+
+xpdf: $(DOCUMENT).pdf
+	xpdf -remote $(DOCUMENT) $(DOCUMENT).pdf
+
+# make the document using the default document class
+$(DOCUMENT).pdf: $(DOCUMENT).tex $(PACKAGES) $(INCLUDES) $(SECTIONS) $(FIGURES)
+
+# housekeeping
+PROJ_CLEAN = $(CLEAN_LATEX)
+PROJ_DISTCLEAN = *.ps *.pdf *.snm *.nav $(PROJ_CLEAN)
+
+# end of file
