61186d10394690f9deb597faed8bc95b7481bd75
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-01-02 11:27:45 -0800

    2010-winter: added the missing makefile

---
+++ 2010-winter/Make.mm
@@ -0,0 +1,39 @@
+# -*- Makefile -*-
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#                             michael a.g. aïvázis
+#                      california institute of technology
+#                      (c) 1998-2010  all rights reserved
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+
+
+PROJECT = acm114
+PACKAGE = 
+
+RECURSE_DIRS = \
+    lectures \
+    assignments \
+    www \
+
+#--------------------------------------------------------------------------
+#
+
+all: tidy export
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+export::
+	BLD_ACTION="export" $(MM) recurse
+
+
+# end of file 
