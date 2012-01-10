a161dfa91341598ebd16d62385dcef46ad38dabf
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-01-09 18:20:24 -0800

    syllabus: removed the printing from {postHomeworkAssignment}

--- syllabus/__init__.py
+++ syllabus/__init__.py
@@ -20,17 +20,6 @@ def postHomeworkAssignment():
     from .PostAssignment import PostAssignment
     # build one
     app = PostAssignment(globalAliases=True)
-    # print its configuration
-    print('post homework assignment')
-    print('  app name: {!r}'.format(app.pyre_name))
-    print('  students: {!r}'.format(tuple(app.selectedStudents())))
-    print('  assignment: {!r}'.format(app.assignment))
-    print('  due date: {!r}'.format(app.due))
-    print('  course: {!r}'.format(app.course.name))
-    print('      home: {!r}'.format(app.course.home))
-    print('      term: {!r}'.format(app.course.term))
-    print('  repository: {!r}'.format(app.repository.pyre_name))
-    print('      executable: {!r}'.format(app.repository.executable))
     # all done
     return app.main()
 
