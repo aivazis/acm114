70ec135a5efd72e9a1a9128cec070f22c381b799
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-04-11 12:16:53 -0700

    2012-spring: build the class website for this quarter

---
+++ 2012-spring/www/Make.mm
@@ -0,0 +1,40 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2012 all rights reserved
+#
+
+
+PROJECT = acm114
+PACKAGE = 2012-spring/www
+WEB = acm114@pyre.caltech.edu:web/$(PACKAGE)
+
+RECURSE_DIRS = \
+    content \
+    images \
+    scripts \
+    styles \
+
+
+#--------------------------------------------------------------------------
+#
+
+all: tidy 
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+export::
+	/usr/bin/rsync --delete -ruavz . $(WEB)
+
+try-export:
+	/usr/bin/rsync -n --delete -ruavz . $(WEB)
+
+# end of file 
---
+++ 2012-spring/www/images/Make.mm
@@ -0,0 +1,22 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2012 all rights reserved
+#
+
+
+PROJECT = acm114
+PACKAGE = 2012-spring/www/images
+WEB = acm114@pyre.caltech.edu:web/$(PACKAGE)
+
+
+#--------------------------------------------------------------------------
+#
+
+all: tidy
+
+export::
+	/usr/bin/rsync --delete -ruavz . $(WEB)
+
+# end of file 
