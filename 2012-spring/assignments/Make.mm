10c24dca7bbabe1816dd34f655863da68a237883
Michael Aïvázis <michael.aivazis@orthologue.com>
2012-04-30 12:57:03 -0700

    2012-spring: composed and posted the {20120507} homework assignment

---
+++ 2012-spring/assignments/Make.mm
@@ -0,0 +1,31 @@
+# -*- Makefile -*-
+#
+# michael a.g. aïvázis
+# california institute of technology
+# (c) 1998-2012 all rights reserved
+#
+
+PROJECT = acm114
+PACKAGE = assignments
+
+RECURSE_DIRS = \
+   assignment-1 \
+
+
+#--------------------------------------------------------------------------
+#
+
+all:
+	BLD_ACTION="all" $(MM) recurse
+
+tidy::
+	BLD_ACTION="tidy" $(MM) recurse
+
+clean::
+	BLD_ACTION="clean" $(MM) recurse
+
+distclean::
+	BLD_ACTION="distclean" $(MM) recurse
+
+
+# end of file
